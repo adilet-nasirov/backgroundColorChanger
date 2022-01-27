e9ad29953b4d48297b1eb6728da0f60cd5cacfca
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-01-27 08:27:59 -0800

    light switch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isOn: false,
+    };
+  }
+
+  render() {
+    const { isOn } = this.state;
+    return (
+      <div>
+        <header>
+          <p style="color: #48a6e8">COLOR FLIPPER</p>
+          <div>
+            <a id="simple">Simple</a>
+            <a id="hex">Hex</a>
+            <a id="rgb">RGB</a>
+          </div>
+        </header>
+        <main>
+          <div class="container">
+            <p style="background-color: black; padding: 15px; border: black solid 2px; border-radius: 10px; margin-bottom: 20px;">
+              Background Color : <span>#F1f5f8</span>
+            </p>
+            <button>CLICK ME</button>
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
